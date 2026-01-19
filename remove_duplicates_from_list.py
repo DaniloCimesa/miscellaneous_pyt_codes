a8ba3d542f1789c969e19897187119a0deb6e700
Danilo Cimesa <danilocimesa3@yahoo.com>
2026-01-19 16:07:47 +0100

    Remove duplicates from a list

---
+++ remove_duplicates_from_list.py
@@ -0,0 +1,10 @@
+#find the unique members of a list
+
+my_list=[1,2,4,4,1,4,2,6,2,9]
+
+unique_l=[]
+for i in my_list:
+    if i not in unique_l:
+        unique_l.append(i)
+            
+print(unique_l)
