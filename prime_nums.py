9098d882b51c2ff0db0e0f74747903a32f8d08fa
Danilo Cimesa <danilocimesa3@yahoo.com>
2026-01-26 17:14:56 +0100

    adding comments

--- prime_nums.py
+++ prime_nums.py
@@ -1,10 +1,15 @@
 
 def is_prime_number(num):
+    #first check if num is less than 2
+    #if so, return False
     if num <=1:
         return False
+    #check if num is divisible by any number from 2 to square root of num+1
     for i in range(2, int(num**0.5) + 1):
+    #if so, return False
         if num % i == 0:
             return False
+    #else return True
     return True
 
 is_prime_number(37)
