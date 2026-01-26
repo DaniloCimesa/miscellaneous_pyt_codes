dd3323026ccc489d55ca92722d7292fd65971e26
Danilo Cimesa <danilocimesa3@yahoo.com>
2026-01-26 12:36:49 +0100

    Sum list function modified

---
+++ sum_list_or_create_and_sum.py
@@ -0,0 +1,22 @@
+def list_sum(lst):
+    """Returns the sum of all elements in the list."""
+    """If input is an integer, then sum all integers from 0 to that integer"""
+    sum=0
+    
+    if type(lst)==list:
+        for elem in lst:
+            sum += elem
+        
+        return sum
+    elif type(lst)==int:
+            for elem in range(lst+1):
+                sum+=elem
+            return sum
+    else:
+        return "Invalid input"
+
+print(list_sum([1,2,3,4,5]))
+
+print(list_sum(9))
+
+print(list_sum("hello"))
