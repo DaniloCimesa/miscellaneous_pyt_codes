06abb37af13588fca8ed32a2e347020a8486104f
Danilo Cimesa <danilocimesa3@yahoo.com>
2026-08-19 18:43:48 +0200

    another way to solve it, with dynamic input of the digits

--- array_sorter.py
+++ array_sorter.py
@@ -25,3 +25,27 @@ no_zeros = [i for i in a if i !=0]
 zeros = (int(len(a)) - int(len(no_zeros)))*[0]
 b = no_zeros + zeros
 print(b)
+
+#v3 of the solution
+def move_zeros_to_end():
+    nums = []
+    print('Enter numbers in a sequence(write Q if you want to quit):')
+    while True:
+        input_num = input('Enter number:')
+    
+        if input_num.lower() == 'q':
+            break
+    
+        if input_num.isdigit() or (input_num.startswith('-') and input_num[1:].isdigit()):
+            nums.append(int(input_num))
+        else:
+            print('Invalid input. Please enter a valid number or Q to quit.')
+    
+    no_zeros = [i for i in nums if i !=0]
+    zeros = [0] * (len(nums) - len(no_zeros))
+    b = no_zeros + zeros
+    return b, nums
+    
+result = move_zeros_to_end()
+print('Original list:', result[1])
+print('List after moving zeros:',result[0])
